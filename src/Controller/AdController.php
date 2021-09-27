3762c3f0ead5c29c520c4f317551460a7fa26e46
Suhard_Noah <noahsuhard78@gmail.com>
2021-09-27 16:55:30 +0200

    27/09/2021

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -2,19 +2,74 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Ad;
+use App\Repository\AdRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdController extends AbstractController
 {
     /**
      * @Route("/ads", name="ads_index")
      */
-    public function index(): Response
+    public function index(AdRepository $repo, SessionInterface $session)
     {
+        //$repo = $this->getDoctrine()->getRepository(Ad::class);
+
+        dump($session);
+
+        $ads = $repo->findAll();
+
         return $this->render('ad/index.html.twig', [
-            'controller_name' => 'AdController',
+            'ads' => $ads
         ]);
     }
+    /**
+     * Permet de créer une annonce
+     * 
+     *@Route("/ads/new", name="ads_create")
+     * 
+     * @return void
+     */
+    public function create(){   
+        $ad = new Ad();
+
+        $form = $this->createFormBuilder($ad)
+                    ->add('title')
+                    ->add('introduction')
+                    ->add('content')
+                    ->add('rooms')
+                    ->add('price')
+                    ->add('coverImage')
+                    ->add('save', SubmitType::class, [
+                        'label' => 'Créer la nouvelle annonce',
+                        'attr' =>[
+                            'class' => 'btn btn-primary'
+                        ]
+                    ])
+                    ->getForm();
+
+        return $this-> render ('ad/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+    /**
+     * Permet d'afficher une seule annonce
+     *
+     * @Route("/ads/{slug}",name="ads_show")
+     * 
+     * @return Response
+     */
+    public function show ($slug, Ad $ad){
+        // //je récupère l'annonce qui correspond au slug !
+        // $ad = $repo ->findOneBySlug($slug);
+
+        return $this->render('ad/show.html.twig',[
+            'ad'=> $ad
+        ]);
+    }
+    
 }
